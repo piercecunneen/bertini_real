7aa0b7c83dde00fcb925d05131e9d44379e55163
piercecunneen <pcunneen19@gmail.com>
2015-11-04 11:50:23 -0500

    Added parsing functions

--- PythonVisualization/Testing.py
+++ PythonVisualization/Testing.py
@@ -1,12 +1,17 @@
 from BRInfo import BRInfo
-from ParsingFunctions import parse_directory_name
+from ParsingFunctions import parse_directory_name, parse_decomposition
 
 
 X = BRInfo()
 directory = parse_directory_name('Dir_Name')
 X.find_directory(directory[0])
-print X.gather_vertices()
-for i in X.vertices:
-    print i
-    print ' '
-    print ' '
+X.gather_vertices()
+X.dimension = directory[2]
+
+
+
+for i in X.gather_surface():
+	print i['midpoint'], i['middle slice index'], i['top'], i['bottom'], i['system top'], i['system bottom'], i['num left'], i['left'], i['num right'], i['right']
+	print ' '
+	print ' ' 
+
